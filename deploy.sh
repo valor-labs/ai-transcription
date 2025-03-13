649add3d8c2d14af8a62ef33b93dfc6e34084f1e
Dima <tgskiv@gmail.com>
2025-03-13 18:58:17 +0200

    Deploy bash minor changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Prompt for Hugging Face token
-read -p "Enter Hugging Face token: " HUGGINGFACE_TOKEN
 
 # Read config file
 CONFIG_FILE="env.tfvars"
@@ -13,7 +11,7 @@ fi
 # Terraform commands
 cd terraform
 terraform init
-terraform apply -var="huggingface_token=$HUGGINGFACE_TOKEN" -var-file="../$CONFIG_FILE"
+terraform apply -var-file="../$CONFIG_FILE"
 
-# # Output results (example)
-# echo "Cloud Run service URL: $(terraform output cloud_run_url)" # Assuming you have this output in Terraform
+# Output results
+echo "Cloud Run service URL: $(terraform output console_url_input_bucket)"
